c736b5f97492157f2a998cfe561d84ea51cb5444
João Vitor de Siqueira Campos <vitor.siqueira@centralit.com.br>
2024-03-27 16:46:51 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = document.querySelector("#qr-form input");
+
+const qrCodeImg = document.querySelector("#qr-code img");
+
+// Eventos
+
+// Gerar QR CODE
+function generateQrCode() {
+  const qrCodeInputValue = qrCodeInput.value;
+
+  if (!qrCodeInput.value) return;
+
+  qrCodeBtn.innerText = "Gerando código...";
+
+  qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+  qrCodeImg.addEventListener("load", () => {
+    container.classList.add("active");
+    qrCodeBtn.innerText = "Código criado!";
+  });
+}
+
+qrCodeBtn.addEventListener("click", () => {
+  generateQrCode();
+});
+
+qrCodeInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    generateQrCode();
+  }
+});
+
+// Limpar area do QR CODe
+qrCodeInput.addEventListener("keyup", () => {
+  if (!qrCodeInput.value) {
+    container.classList.remove("active");
+    qrCodeBtn.innerText = "Gerar QR Code";
+  }
+});
